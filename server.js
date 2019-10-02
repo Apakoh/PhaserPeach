f621206ceacc3a746d3ffa7feed7956bf16f4ccb
Apakoh <quentin.bartschs@gmail.com>
2019-10-02 17:58:32 +0200

    Init Project

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const server = express();
+const port = 3000;
+const address = "0.0.0.0";
+
+server.use("/Assets", express.static(__dirname + '/Assets'));
+
+server.engine('html', require('atpl').__express);
+server.set('devel', false);
+server.set('view engine', 'html');
+server.set('view cache', false);
+server.set('views', __dirname);
+
+var bodyParser = require('body-parser');
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+
+
+async function run()
+{
+    // Gestion of pages
+    server.get('/', function(req, res){
+      res.render('Peach.html');
+    });
+    server.get('/Peach.html', function(req, res){
+      res.render('Peach.html');
+    });
+    server.listen(process.env.PORT || port, address, function() {
+        console.log('Listening to port:  ' + port);
+    });
+
+}
+
+run();
